51e488753da128207f071c1878da5fb559a21747
celly <celly@livesport.eu>
2016-09-15 11:40:43 +0200

    Uprava pro potreby LS

--- picast.js
+++ picast.js
@@ -3,19 +3,54 @@ var exec = require('child_process').exec;
 var express = require('express');
 var app = express();
 
+var lastProcess = null;
+
+var startProcess = function(cmd) {
+	stopProcess();
+	lastProcess = exec(cmd);
+	// lastProcess.stdout.on('data', function (data) {
+	//   console.log("1" + data);
+	// });
+
+	// lastProcess.stderr.on('data', function (data) {
+	//   console.log("2" + data);
+	// });
+
+	// lastProcess.on('exit', function (code) {
+	//   console.log("Child exited with code " + code);
+	// });
+};
+
+var stopProcess = function() {
+	if (lastProcess) {
+		lastProcess.kill('SIGHUP');
+		lastProcess = null;
+	}
+};
+
 app.get('/', function (req, res) {
-        res.send('Welcome to PiCAST 3! In the URL, type what you want to do...');
+	res.send('Welcome to PiCAST 3! In the URL, type what you want to do...');
 });
 
-app.get('/yt-stream/:url', function (req, res) {
-        res.send('Streaming YouTube Video...');
-        exec("livestreamer --player=mplayer https://www.youtube.com/watch?v=" + req.params.url + " best");
+app.get('/stop', function (req, res) {
+	stopProcess();
+	res.send('Stop');
 });
+
+app.get('/start', function (req, res) {
+	res.send('Start');
+	startProcess("ls");
+});
+
+// app.get('/yt-stream/:url', function (req, res) {
+//		 res.send('Streaming YouTube Video...');
+//		 exec("livestreamer --player=mplayer https://www.youtube.com/watch?v=" + req.params.url + " best");
+// });
 
 // Setup PiCAST Server
 var srv = app.listen(3000, function () {
-        var host = srv.address().address;
-        var port = srv.address().port;
+		var host = srv.address().address;
+		var port = srv.address().port;
 
-        console.log('Access at http://%s:%s', host, port);
+		console.log('Access at http://%s:%s', host, port);
 });
